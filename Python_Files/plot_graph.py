918a2392339b9c3b588cd8ff8bf348aa3c241915
Shivam Sharma <15UCC035@lnmiit.ac.in>
2017-12-08 20:08:10 +0530

    Adding Graphs

--- Python_Files/plot_graph.py
+++ Python_Files/plot_graph.py
@@ -2,7 +2,7 @@ import Image
 import numpy as np
 import matplotlib.pyplot as plt
 
-time_loss = np.load('../Extracted_Features/Time_Loss_Read.npy')
+time_loss = np.load('../Extracted_Features/Time_Loss_Numeric.npy')
 
 x_labels, y_labels = [], []
 
@@ -11,5 +11,5 @@ for vec in time_loss:
     y_labels.append(vec[0])
 
 plt.plot(x_labels, y_labels)
-plt.savefig('../graphs/Read.png')
-Image.open('../graphs/Read.png').save('../graphs/Read.jpg','JPEG')
+plt.savefig('../graphs/Numeric.png')
+Image.open('../graphs/Numeric.png').save('../graphs/Numeric.jpg','JPEG')
