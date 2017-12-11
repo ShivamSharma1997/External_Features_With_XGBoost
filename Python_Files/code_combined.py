a402adf1576972246fe045d85eb9d0548dad1d6e
Shivam Sharma <15UCC035@lnmiit.ac.in>
2017-12-11 16:02:00 +0530

    Update 11/12/2017

--- Python_Files/code_combined.py
+++ Python_Files/code_combined.py
@@ -5,7 +5,8 @@ from utility import xgb_bst
 feat_lex = np.load('../Extracted_Features/lex.npy')
 feat_read = np.load('../Extracted_Features/read.npy')
 feat_numeric = np.load('../Extracted_Features/numeric.npy')
+label = np.load('../Extracted_Features/label.npy')
 
 feat = np.hstack((feat_lex,feat_read, feat_numeric))
 
-print 'Best for combined:', xgb_bst(feat)
+print 'Best for combined:', xgb_bst(feat, label)
